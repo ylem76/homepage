86793b141aefad0185bfb547051418fc22c4165e
ylem76 <ylem76@gmail.com>
2020-08-13 22:44:52 +0900

    sonosa scss commit

--- sonosa_scss/dist/main.fb6bbcaf.js
+++ sonosa_scss/dist/main.fb6bbcaf.js
@@ -119,7 +119,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
   return newRequire;
 })({"js/main.js":[function(require,module,exports) {
 
-},{}],"../../../../../AppData/Roaming/npm-cache/_npx/12712/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{}],"../../../../../AppData/Roaming/npm-cache/_npx/4492/node_modules/parcel/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -147,7 +147,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64747" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53963" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -323,5 +323,5 @@ function hmrAcceptRun(bundle, id) {
     return true;
   }
 }
-},{}]},{},["../../../../../AppData/Roaming/npm-cache/_npx/12712/node_modules/parcel/src/builtins/hmr-runtime.js","js/main.js"], null)
+},{}]},{},["../../../../../AppData/Roaming/npm-cache/_npx/4492/node_modules/parcel/src/builtins/hmr-runtime.js","js/main.js"], null)
 //# sourceMappingURL=/main.fb6bbcaf.js.map
